abe7c877823c8ef4f0ee66aa104bbecd47daa242
Renny <100791149+I4M4N4N0DE@users.noreply.github.com>
2022-10-31 17:23:44 +0100

    Add files via upload

---
+++ adviser.py
@@ -0,0 +1,121 @@
+import random as Ran
+from colorama import Fore
+import art as Art # this library has to be installed
+
+def advise(): # the basic adviser function
+    
+    answersOnQuestion = ["Sure.", "I wouldn't rely on that.",
+                         "Maybe.", "That? Definitely not."]
+    
+    welcome = Art.text2art("ADVISER")
+    print(Fore.MAGENTA + welcome)
+    
+    # functional part 
+    while True:
+
+        question = input(Fore.LIGHTGREEN_EX + "Enter your question: ")
+    
+        if question.endswith("?"):
+            choice = Ran.choice(answersOnQuestion)
+            print(choice)
+            print("")
+            
+            while True:
+                repChoice = input("Restart? [Y/N]")
+                if repChoice == "Y":
+                    advise()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print("Didn't understand - again: ")
+        else:
+            print(Fore.RED + "That wasn't a question - again: ")           
+        
+def confession(): # the confessing function
+    
+    ok = """ ___________                  
+            < IT'S OK. >
+             ----------- 
+                    \   ^__^
+                     \  (oo)\_______
+                        (__)\       )\/
+                            ||----w |
+                            ||     ||
+                                        """
+        
+    notok = """ ___________ 
+              < IT'S NOT OK. >
+                ----------- 
+                    \   ^__^
+                     \  (oo)\_______
+                        (__)\       )\/
+                            ||----w |
+                            ||     ||
+                                        """
+    
+    welcome = Art.text2art("CONFESSOR")
+    print(Fore.CYAN + welcome)
+    
+    # this is a database of all bad words significating a bad situation
+    # it can be customized or updated any time
+    badwords = ["stole", "steal", "stolen",
+                "hurt", "robbered", "robbery",
+                "robber", "hit", "punched",
+                "punch", "blood", "bleed", "bleeded",
+                "bleedy", "bleeding", "bloody",
+                "wound", "attack", "attacked"]
+    
+    # functional part
+    while True:
+        
+        message = input(Fore.LIGHTRED_EX + "What's on your heart? ")
+        
+        if any(word in message for word in badwords):
+            print(Fore.RED + notok)
+            print("")
+            print("Well, that's not good at all. Solve it as fairly as it has to be.")
+            print("Having cold and calmed brain is most important.")
+            
+            while True:
+                repChoice = input("Restart? [Y/N]")
+                if repChoice == "Y":
+                    confession()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print("Didn't understand - again: ")
+        else:
+            print(Fore.LIGHTGREEN_EX + ok)
+            print("")
+            print("It's ok.")
+            print("If you're not happy about how it is, keep your head calmed, determine the situation and solve it calmly.")
+            print("If you're happy with it, there's no reason for solving anything :).")
+       
+            while True:
+                repChoice = input("Restart? [Y/N]")
+                if repChoice == "Y":
+                    confession()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print(Fore.RED + "Didn't understand - again: ")
+
+# welcomming function choosing the working mode
+def main():
+    
+    print("Welcome in the program.")
+    print("Do you want to talk with adviser, or with confessor?")
+    print("Adviser = 0,")
+
+    mainChoice = int(input("confessor = 1: "))
+    
+    while True:
+        
+        if mainChoice == 0:
+            advise()
+        elif mainChoice == 1:
+            confession()
+        else:
+            print("Didn't understand - again: ")
+
+main()
---
+++ adviserCZ.py
@@ -0,0 +1,135 @@
+import random as Ran
+from colorama import Fore
+import art as Art # tuto knihovnu je nutné mít nainstalovanou
+
+def porada(): # funkce jednoduchého poradce
+    
+    answersOnQuestion = ["Určitě.", "Nespoléhal bych na to.",
+                         "Možná.", "To rozhodně ne."] # seznam primitivních odpovědí na otázku
+    
+    welcome = Art.text2art("PORADCE")
+    print(Fore.MAGENTA + welcome)
+    
+    # funkční část
+    while True:
+
+        question = input(Fore.LIGHTGREEN_EX + "Zadej tvůj dotaz: ")
+    
+        if question.endswith("?"):
+            choice = Ran.choice(answersOnQuestion)
+            print(choice)
+            print("")
+            
+            while True:
+                repChoice = input("Restartovat? [A/N]")
+                if repChoice == "A":
+                    porada()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print("Nerozumím - znovu: ")
+        else:
+            print(Fore.RED + "To nebyla otázka, znovu prosím: ")           
+        
+def zpoved(): # funkce zpovědnice
+    
+    ok = """ ___________                  
+            < JE TO OK. >
+             ----------- 
+                    \   ^__^
+                     \  (oo)\_______
+                        (__)\       )\/
+                            ||----w |
+                            ||     ||
+                              Z          """
+        
+    notok = """ ___________ 
+              < NENÍ TO OK. >
+                ----------- 
+                    \   ^__^
+                     \  (oo)\_______
+                        (__)\       )\/
+                            ||----w |
+                            ||     ||
+                                        """
+    
+    welcome = Art.text2art("ZPOVEDNICE")
+    print(Fore.CYAN + welcome)
+    
+    # databáze slov, které určují špatnou situaci 
+    # může být libovolně upravována, nebo doplňována
+    badwords = ["ukradl", "Ukradl", "ukradla", "Ukradla", "ublížil",
+                "Ublížil", "ublížila", "Ulbížila", "ublížilo",
+                "Ublížilo", "ublížili", "Ublížili", "ublížily",
+                "Ublížily", "ukradli", "Ukradli", "ukradlo",
+                "Ukradlo", "ukradly", "Ukradly", "odcizily",
+                "Odcizily", "odZený", "Uhozený", "praštil",
+                "Praštil", "praštila", "Praštila", "praštili",
+                "Praštili", "praštily", "Praštily", "přaštěn",
+                "Praštěn", "praštěni", "Praštěni", "praštění",
+                "Praštění", "krev", "Krev", "krvácel", "Krvácel",
+                "krvácela", "Krvácela", "rána", "Rána"
+                "napadl", "napadla", "Napadl", "Napadla",
+                "uhodil", "Uhodil", "uhodila", "Uhodila",
+                "ukrást", "Ukrást", "ublížit", "Ublížit",
+                "odcizit", "Odcizit", "uhodit", "Uhodit",
+                "praštit", "Praštit", "krvácet", "Krvácet",
+                "Krvácí", "krvácí", "napadat", "Napadat",
+                "ukraden", "Ukraden", "ukradena", "Ukradena",
+                "okrást", "Okrást", "okraden", "Okraden",
+                "Okradena", "okrZdena"]
+    
+    # funkční část
+    while True:
+        
+        message = input(Fore.LIGHTRED_EX + "Copak ti leží na srdci? ")
+        
+        if any(word in message for word in badwords):
+            print(Fore.RED + notok)
+            print("")
+            print("Tak tohle není dobrý, musíš situaci vyřešit správně a spravedlivě.")
+            print("Důležitá je chladná hlava, ale jednej, protože to je třeba vyřešit.")
+            
+            while True:
+                repChoice = input("Restartovat? [A/N]")
+                if repChoice == "A":
+                    zpoved()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print("Nerozumím - znovu: ")
+        else:
+            print(Fore.LIGHTGREEN_EX + ok)
+            print("")
+            print("Je to v pořádku.")
+            print("Pokud s tím nejsi spokojen, zachovej chladnou hlavu, zhodnoť stav situace a jednej v klidu.")
+            print("Pokud jsi spokojen, není co řešit :).")
+            
+            while True:
+                repChoice = input("Restartovat? [A/N]")
+                if repChoice == "A":
+                    zpoved()
+                elif repChoice == "N":
+                    quit()
+                else:
+                    print("Nerozumím - znovu: ")
+
+# úvodní funkce volící pracovní režim
+def main():
+    
+    print("Vítej v programu.")
+    print("Chceš mluvit s poradcem, nebo se zpovědníkem?")
+    print("Poradce = 0,")
+
+    mainChoice = int(input("zpovědnice = 1: "))
+    
+    while True:
+        
+        if mainChoice == 0:
+            porada()
+        elif mainChoice == 1:
+            zpoved()
+        else:
+            print("Nerozumím - ještě jednou: ")
+
+main()
